ab98bd689e2df32fe82a10a1b4b921c3e3c24de1
Khushi Jaiswal <104351996+KHUSHIJAISWAL2608@users.noreply.github.com>
2023-05-19 23:09:52 +0530

    Add files via upload

---
+++ countvowel.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+// #include <stdio.h>
+
+#include <conio.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char str[100];
+    int i,vowCount=0,consCount=0;
+    cout<<"Enter a string for count vowel and consonant\n";
+    gets(str);//store the input from user
+    for(i=0; str[i]; i++){
+        if(str[i] == 'a'|| str[i] == 'e'|| str[i] == 'i'|| str[i] == 'o'|| str[i] == 'u'
+    ||str[i] == 'A'|| str[i] == 'E'|| str[i] == 'I'|| str[i] == 'O'|| str[i] == 'U' ){
+        vowCount++;//vowel counter incremented by 1
+    }
+    else {
+        consCount++;//consonant counter incremented by 1
+    }
+}
+cout<<"Number of vowels:"<<vowCount<<endl;//display number of vowels
+cout<<"Number of consonants:"<<consCount<<endl;//display number of consonant
+getch();
+    return 0;
+}
