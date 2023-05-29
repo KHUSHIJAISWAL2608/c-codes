0a61e9476124d50cbf81ceaa0006c057edd5534a
Khushi Jaiswal <104351996+KHUSHIJAISWAL2608@users.noreply.github.com>
2023-05-29 23:19:13 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+// int sum(int a, int b)  function prototype
+int sum(int a, int b){
+    int c=a+b;
+    return c;
+
+}
+int num1,num2;
+int main(){
+cout<<"enter the value of num1"<<endl;
+cin>>num1;
+
+cout<<"Enter the value of num2<<"<<endl;
+cin>>num2;
+cout<<"Sum of the values of num1+num2"<<sum(num1, num2);  
+void g ();
+}
+void P(){
+    cout<<"Hello Goodmorning!!"<<endl;
+
+return 0;
+}
